ab29e1900675e458e598cd3321bc3744386167c1
Yoel Ivan <yivan3@gatech.edu>
2016-09-14 17:23:40 +0100

    init commit

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,16 @@
+/* @flow */
+"use strict";
+
+import React, { Component } from "react";
+
+class Welcome extends Component {
+    render() {
+        return (
+            <div className="ui segment">
+                WELCOME!
+            </div>
+        );
+    }
+}
+
+export default Welcome;
