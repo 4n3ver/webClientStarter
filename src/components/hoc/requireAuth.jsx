8a7b409b55cf843296b637669a4117c4e8e79eda
4n3ver <4n3ver@reborn.com>
2016-09-16 23:21:38 +0100

    fix missing semi colon in requireAuth.jsx

--- src/components/hoc/requireAuth.jsx
+++ src/components/hoc/requireAuth.jsx
@@ -1,3 +1,4 @@
+/* @flow */
 "use strict";
 
 import React, { Component } from "react";
@@ -20,7 +21,7 @@ export default (ComposedComponent) => {
         render() {
             return (
                 <ComposedComponent {...this.props} />
-            )
+            );
         }
     }
 
